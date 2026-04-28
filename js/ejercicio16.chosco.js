394050423f1d599d8f53748a51310b968a8ba553
Chosco Fabricio <fabrisiochosco38@gmail.com>
2026-04-28 15:46:20 -0300

    Crear funcion sumarrango Ej 16 Chosco
    contamos con un acumulador, y con for hacemos la suma del numero inicial y final

--- js/ejercicio16.chosco.js
+++ js/ejercicio16.chosco.js
@@ -1,2 +1,11 @@
 const inicio = parseInt(prompt("Ingrese el número inicial:"));
 const fin = parseInt(prompt("Ingrese el número final:"));
+const sumarRango = (numeroInicial, numeroFinal) => {
+    let suma = 0;
+
+    for (let i = numeroInicial; i <= numeroFinal; i++) {
+        suma += i;
+    }
+
+    return suma;
+};
