c5afdfdd9813b4b0f05e45ea4c712704a59e7b9c
Chosco Fabricio <fabrisiochosco38@gmail.com>
2026-04-28 15:37:32 -0300

    Creamos funcion duplicar
    creamos un nuevarreglo, duplica el numero, guarda el valor y lo retorna

--- js/ejercicio14.chosco.js
+++ js/ejercicio14.chosco.js
@@ -6,3 +6,17 @@ for (let i = 0; i < cantidad; i++) {
     let numero = parseFloat(prompt(`Ingrese el numero ${i + 1}:`));
     numeros.push(numero);
 }
+const duplicar = (arreglo) => {
+    let nuevoArreglo = [];
+
+    for (let i = 0; i < arreglo.length; i++) {
+        nuevoArreglo.push(arreglo[i] * 2);
+    }
+
+    return nuevoArreglo;
+};
+
+const resultado = duplicar(numeros);
+
+console.log(`Arreglo original: ${numeros}`);
+console.log(`Arreglo duplicado: ${resultado}`);
