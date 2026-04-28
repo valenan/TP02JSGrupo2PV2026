0e099d98c9ef591ddcf6ab77081dfb97f98b3510
franclyc <francoguanactolay61@gmail.com>
2026-04-28 14:21:24 -0300

    Terminamos el codigo ya funcionando Guanactolay Franco

--- js/ejercico4.js
+++ js/ejercico4.js
@@ -4,3 +4,9 @@ const altura = 8;
 const calcularArea = (b, h) => b * h;
 const calcularPerimetro = (b, h) => 2 * (b + h);
 
+const area = calcularArea(base, altura);
+const perimetro = calcularPerimetro(base, altura);
+
+console.log(`Rectangulo con base ${base} cm y altura ${altura} cm`);
+console.log(`Area: ${area} cm²`);
+console.log(`Perimetro: ${perimetro} cm`);
