304347044567d361654a4338e533065b0f6eebb3
Chosco Fabricio <fabrisiochosco38@gmail.com>
2026-04-28 14:37:40 -0300

    Mejorae formato para salida enumeraa Ej7 Chosco Fabricio
    Reemplace por console.log(`${i + 1}. ${nombres[i]}`);

--- ejercicio7.chosco.js
+++ ejercicio7.chosco.js
@@ -3,5 +3,5 @@ const nombres = ["Lucas", "Martina", "Sofia", "Alejandro", "Valentina", "Juan"];
 console.log("Lista de nombres: "); 
 
 for (let i = 0; i < nombres.length; i++) {
-    console.log(nombres[i]);
+    console.log(`${i + 1}. ${nombres[i]}`);
 }
