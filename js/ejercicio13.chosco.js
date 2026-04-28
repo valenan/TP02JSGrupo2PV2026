aaba1e8f555d752340d21bb68a28fa7890d59e60
Chosco Fabricio <fabrisiochosco38@gmail.com>
2026-04-28 15:26:13 -0300

    Funcion mostrararreglo e imprimir por consola Ej 13 Chosco
    Uso funcion flecha. recibe el array con la cantidad de elementos, imprime uno por linea y despues llamamos a la funcion con el arreglo ya cargao

--- js/ejercicio13.chosco.js
+++ js/ejercicio13.chosco.js
@@ -6,3 +6,13 @@ for (let i = 0; i < cantidad; i++) {
     let numero = parseFloat(prompt(`Ingrese el numero ${i + 1}:`));
     numeros.push(numero);
 }
+// funcion flecha
+const mostrarArreglo = (arreglo) => {
+    console.log("Números ingresados:");
+
+    for (let i = 0; i < arreglo.length; i++) {
+        console.log(arreglo[i]);
+    }
+};
+
+mostrarArreglo(numeros);
