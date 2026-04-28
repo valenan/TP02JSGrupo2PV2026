ff934174abf460ebbdc4bb8f42468c4a2b736556
Chosco Fabricio <fabrisiochosco38@gmail.com>
2026-04-28 14:20:18 -0300

    Array y mostrar los nombres
    Cree un arreglo con los 6 nombres y con el for recorro todos los nomnres

---
+++ ejercicio7.chosco.js
@@ -0,0 +1,7 @@
+const nombres = ["Lucas", "Martina", "Sofia", "Alejandro", "Valentina", "Juan"];
+
+console.log("Lista de nombres: "); 
+
+for (let i = 0; i < nombres.length; i++) {
+    console.log(nombres[i]);
+}
